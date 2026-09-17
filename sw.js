1196407514c73c2e726f56a300e866a864f6e226
yob-code <284639681+yob-code@users.noreply.github.com>
2026-09-17 21:03:13 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE = 'sticky-v15';
+const CACHE = 'sticky-v16';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', e => {
